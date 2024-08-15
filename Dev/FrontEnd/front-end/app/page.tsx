d5eba02a3602c58becb5a8f036975648bfd73633
ThyPirateKing <pirateking_nick@yahoo.com>
2024-08-15 18:10:57 -0400

    Fixed the error of finding the module for Login.

--- Dev/FrontEnd/front-end/app/page.tsx
+++ Dev/FrontEnd/front-end/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import styles from "./page.module.css";
-import { Login } from "@/app/components/Login/Login";
+import Login from "@/components/Login/Login";
 export default function Home() {
   return (
     <main className={styles.main}>
