f06c312f132f565d880b0b1a08436abc8464fcb4
Chike Egbuchulam <egbuchulamchike@gmail.com>
2024-08-16 17:15:01 -0400

    First file adds

---
+++ Dev/Services/ItemServices.cs
@@ -0,0 +1,40 @@
+namespace Services;
+
+using Models;
+using Repository;
+public class ItemServices
+{
+    private DataContext _context;
+    private ItemRepository repo;
+    // Constructor
+    public Item Create(ItemRepository repo)
+    {
+        this.repo = repo;
+    }
+    // Get All
+    public List<Item> GetAll()
+    {
+        return repo.List();
+    }
+    // Get By ID
+    public Item GetById(int id)
+    {
+        return repo.GetById(id);
+    }
+    // Save/Create
+    public Item Save(Item model)
+    {
+        return repo.Save(model);
+    }
+    // Delete by ID
+    public Item DeleteByID(int id)
+    {
+        Item target = repo.GetById(id);
+        return repo.Delete(model);
+    }
+    // Update
+    public Item Update(int id, Item model)
+    {
+        return repo.Update(id, model);
+    }
+}
