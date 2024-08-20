df35462b100bc955e6a3a8aae4f80ff6e816cc35
ThyPirateKing <pirateking_nick@yahoo.com>
2024-08-20 18:05:13 -0400

    Added bootstrap styling to the navigation bar and the login page.

--- Dev/FrontEnd/front-end/components/Navbar/Navbar.tsx
+++ Dev/FrontEnd/front-end/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import NavbarStyles from "./Navbar.module.css";
 import Login from "@/components/Login/Login";
 import AccountMenu from "@/app/AccountMenu/page";
 import Link from 'next/link';
-function Navbar({showLogin, setShowLogin, isLogin, setIsLogin, isAccountClick, setIsAccountClick} : {showLogin: boolean, setShowLogin: any, isLogin: boolean, setIsLogin: any, isAccountClick: boolean, setIsAccountClick: any})
+const Navbar = ({showLogin, setShowLogin, isLogin, setIsLogin, isAccountClick, setIsAccountClick} : {showLogin: boolean, setShowLogin: any, isLogin: boolean, setIsLogin: any, isAccountClick: boolean, setIsAccountClick: any}) =>
 {
     const handleLogin = () => {
         if(showLogin)
@@ -25,14 +25,59 @@ function Navbar({showLogin, setShowLogin, isLogin, setIsLogin, isAccountClick, s
         if(isAccountClick)
         {
             setIsAccountClick(false);
+            setShowLogin(true);
         }
         else
         {
             setIsAccountClick(true);
+            setShowLogin(false);
         }
     }
 
+    return (
+        <nav className="navbar navbar-light bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand">Navbar</a>
+            <form className="d-flex input-group w-auto">
+              <input
+                type="search"
+                className="form-control   
+     rounded"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="search-addon"
+              />
+              <span className="input-group-text border-0" id="search-addon">   
+    
+                <i className="fas fa-search"></i>
+              </span>
+            </form>
+            <div className="d-flex align-items-center">   
+    
+              {isLogin ? (
+                <>
+                  <button onClick={handleAccountButton} className="btn btn-primary">
+                    Account
+                  </button>
+                  <button onClick={handleLogin} className="btn btn-primary">
+                    Logout
+                  </button>
+                </>
+              ) : (
+                <button onClick={handleLogin} className="btn btn-primary">
+                  Login
+                </button>
+              )}
+            </div>
+          </div>
+        </nav>
+      );
+    };
+    
+    
+    
 
+/*
     return (
 
 <div className={NavbarStyles.navbar}>
@@ -57,5 +102,6 @@ function Navbar({showLogin, setShowLogin, isLogin, setIsLogin, isAccountClick, s
         </div>
       );
     }
+      */
 
 export default Navbar;
