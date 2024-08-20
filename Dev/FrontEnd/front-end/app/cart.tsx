f2a135d28676d1eacb512dac9e3ee0f12afc05c8
ThyPirateKing <pirateking_nick@yahoo.com>
2024-08-20 09:45:56 -0400

    Added more components for various aspects of the web app such as the cart and an account menu.

---
+++ Dev/FrontEnd/front-end/app/cart.tsx
@@ -0,0 +1,11 @@
+function Cart()
+{
+    return(
+        <div>
+            <input type="text" placeholder="Email"></input>
+            <input type="password" placeholder="Password"></input>
+        </div>
+    )
+};
+
+export default Cart;
--- Dev/FrontEnd/front-end/components/AccountMenu/AccountMenu.tsx
+++ Dev/FrontEnd/front-end/components/AccountMenu/AccountMenu.tsx
@@ -1,11 +1,20 @@
 import AccountMenuStyles from "./AccountMenuStyles.module.css";
-function AccountMenu()
-{
-    return(
+//import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Link from 'next/link';
+import { useRouter } from "next/router";
+import Cart from "../Cart/Cart";
+function AccountMenu() {
+    //const router = useRouter();
+
+    const createRouter = () => {
+        window.location.href = "/cart";
+    }
+
+    return (
         <div className={AccountMenuStyles.accountMenu}>
+            <Link href = "/cart">Cart</Link>
             <p>Manage Account</p>
             <p>Orders</p>
-            <p>Cart</p>
         </div>
     )
 };
