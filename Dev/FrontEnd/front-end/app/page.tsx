33772c99892d24163580824cd3a7b839d493f9e2
ThyPirateKing <pirateking_nick@yahoo.com>
2024-08-21 18:11:21 -0400

    Added a registration form to display when clicking on the registration button on the login page.

--- Dev/FrontEnd/front-end/app/page.tsx
+++ Dev/FrontEnd/front-end/app/page.tsx
@@ -6,35 +6,43 @@ import Login from "@/components/Login/Login";
 import Listing from "@/components/Listing/Listing"
 import { products } from "@/components/Listing/listings.js"
 import AccountMenu from "@/components/AccountMenu/AccountMenu";
+import RegistrationForm from "@/components/RegistrationForm/RegistrationForm";
 
 export default function Home() {
   const [isLogin, setIsLogin] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
   const [isAccountClick, setIsAccountClick] = useState(false);
+  const [registrationClick, setRegistrationClick] = useState(false);
 
   return (
     <main className={styles.main}>
 
+      {/* <RegistrationForm /> */}
+
       {!showLogin ? (
         <>
 
-        <Navbar showLogin={showLogin} setShowLogin={setShowLogin} isLogin={isLogin} setIsLogin={setIsLogin}
-          isAccountClick={isAccountClick} setIsAccountClick={setIsAccountClick} />
-        
-      <div className={styles.listing}>
-        {products.map((item, key) => {
-          return (
-            <Listing key={key} title={item.title} image={item.image} price={item.price} />
-          )
-        })}
-      </div>
+          <Navbar showLogin={showLogin} setShowLogin={setShowLogin} isLogin={isLogin} setIsLogin={setIsLogin}
+            isAccountClick={isAccountClick} setIsAccountClick={setIsAccountClick} />
 
-      {isAccountClick && <AccountMenu />}
-      
-      </>
+          <div className={styles.listing}>
+            {products.map((item, key) => {
+              return (
+                <Listing key={key} title={item.title} image={item.image} price={item.price} />
+              )
+            })}
+          </div>
 
-      ) : (<Login showLogin={showLogin} setShowLogin={setShowLogin} isLogin={isLogin} setIsLogin={setIsLogin}
-        isAccountClick={isAccountClick} setIsAccountClick={setIsAccountClick} />)}
+          {isAccountClick && <AccountMenu />}
+
+        </>
+
+      ) : (
+
+        <Login showLogin={showLogin} setShowLogin={setShowLogin} isLogin={isLogin} setIsLogin={setIsLogin}
+          isAccountClick={isAccountClick} setIsAccountClick={setIsAccountClick} registrationClick={registrationClick} setRegistrationClick={setRegistrationClick} />
+
+      )}
     </main>
   );
 }
--- Dev/FrontEnd/front-end/components/Login/Login.tsx
+++ Dev/FrontEnd/front-end/components/Login/Login.tsx
@@ -1,9 +1,9 @@
 import LoginStyles from "./Login.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import RegistrationForm from "../RegistrationForm/RegistrationForm";
 
-
-const Login = ({ showLogin, setShowLogin, isLogin, setIsLogin, isAccountClick, setIsAccountClick }:{ showLogin: boolean, setShowLogin: any, isLogin: boolean, setIsLogin: any, isAccountClick: boolean, setIsAccountClick: any }) => {
+const Login = ({ showLogin, setShowLogin, isLogin, setIsLogin, isAccountClick, setIsAccountClick, registrationClick, setRegistrationClick }: { showLogin: boolean, setShowLogin: any, isLogin: boolean, setIsLogin: any, isAccountClick: boolean, setIsAccountClick: any, registrationClick: boolean, setRegistrationClick: any }) => {
 
     const handleLogin = () => {
         if (showLogin) {
@@ -19,121 +19,98 @@ const Login = ({ showLogin, setShowLogin, isLogin, setIsLogin, isAccountClick, s
         }
     };
 
+    const handleRegistrationClick = () => {
+        setRegistrationClick(true);
+    };
+
     return (
-        <section className="vh-100" style={{ backgroundColor: '#9A616D' }}>
-            <div className="container py-5 h-100">
+        <>
+            {!registrationClick ? (
 
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col col-xl-10">
-                        <div className="card" style={{ borderRadius: '1rem' }}>
-                            <div className="row g-0">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img
-                                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                <section className="vh-100" style={{ backgroundColor: '#9A616D' }}>
+                    <div className="container py-5 h-100">
 
-                                        alt="login form"
-                                        className="img-fluid"
-                                        style={{ borderRadius: '1rem 0 0 1rem' }}
-                                    />
-                                </div>
-                                <div className="col-md-6   
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col col-xl-10">
+                                <div className="card" style={{ borderRadius: '1rem' }}>
+                                    <div className="row g-0">
+                                        <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                            <img
+                                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+
+                                                alt="login form"
+                                                className="img-fluid"
+                                                style={{ borderRadius: '1rem 0 0 1rem' }}
+                                            />
+                                        </div>
+                                        <div className="col-md-6   
    col-lg-7 d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-black">
-                                        <form>
-                                            <div className="d-flex align-items-center mb-3 pb-1">
-                                                <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
-                                                <span className="h1 fw-bold mb-0">Logo</span>
+                                            <div className="card-body p-4 p-lg-5 text-black">
+                                                <form>
+                                                    <div className="d-flex align-items-center mb-3 pb-1">
+                                                        <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
+                                                        <span className="h1 fw-bold mb-0">Logo</span>
 
+                                                    </div>
+
+                                                    <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>
+                                                        Sign into your account
+                                                    </h5>
+
+                                                    <div className="form-outline mb-4">
+                                                        <input
+                                                            type="email"
+                                                            id="form2Example17"
+                                                            className="form-control form-control-lg"
+
+                                                            placeholder="Email address"
+                                                        />
+                                                    </div>
+
+                                                    <div className="form-outline mb-4">
+                                                        <input
+                                                            type="password"
+                                                            id="form2Example27"
+
+                                                            className="form-control form-control-lg"
+                                                            placeholder="Password"
+                                                        />
+                                                    </div>
+
+                                                    <div className="pt-1 mb-4">
+
+                                                        <button className="btn btn-dark btn-lg btn-block" type="submit" onClick={handleLogin}>
+                                                            Login
+                                                        </button>
+                                                    </div>
+
+                                                    <a className="small text-muted" href="#!">
+                                                        Forgot password?
+                                                    </a>
+                                                    <p className="mb-5 pb-lg-2"
+                                                        style={{ color: '#393f81' }}>
+                                                        Don't have an account? <button onClick={handleRegistrationClick} className="btn btn-link" style={{ color: '#393f81' }}>
+                                                            Register here
+                                                        </button>
+                                                    </p>
+                                                    <a href="#!" className="small text-muted">
+                                                        Terms of use.
+                                                    </a>
+                                                    <a href="#!" className="small text-muted">
+                                                        Privacy policy
+                                                    </a>
+                                                </form>
                                             </div>
-
-                                            <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>
-                                                Sign into your account
-                                            </h5>
-
-                                            <div className="form-outline mb-4">
-                                                <input
-                                                    type="email"
-                                                    id="form2Example17"
-                                                    className="form-control form-control-lg"
-
-                                                    placeholder="Email address"
-                                                />
-                                            </div>
-
-                                            <div className="form-outline mb-4">
-                                                <input
-                                                    type="password"
-                                                    id="form2Example27"
-
-                                                    className="form-control form-control-lg"
-                                                    placeholder="Password"
-                                                />
-                                            </div>
-
-                                            <div className="pt-1 mb-4">
-
-                                                <button className="btn btn-dark btn-lg btn-block" type="submit" onClick={handleLogin}>
-                                                    Login
-                                                </button>
-                                            </div>
-
-                                            <a className="small text-muted" href="#!">
-                                                Forgot password?
-                                            </a>
-                                            <p className="mb-5 pb-lg-2"
-                                                style={{ color: '#393f81' }}>
-                                                Don't have an account? <a href="#!" style={{ color: '#393f81' }}>Register here</a>
-                                            </p>
-                                            <a href="#!" className="small text-muted">
-                                                Terms of use.
-                                            </a>
-                                            <a href="#!" className="small text-muted">
-                                                Privacy policy
-                                            </a>
-                                        </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </section>
-
+                </section>
+            ) : (<RegistrationForm />)
+            };
+        </>
     );
 };
-
-/*
-export default Login;   
- 
-
-function Login() {
-  return (
-
-
-
-      
-
-      
-
-
-
-          <div className={LoginStyles.login}>
-
-<div className={LoginStyles.container}>
-
-              <h1 className={LoginStyles.title} >Welcome Back</h1>
-
-              
-              <input type="text" placeholder="Email"></input>
-              <input type="password" placeholder="Password"></input>
-          </div>
-
-          
-
-      </div>
-  )
-};
-*/
-
 export default Login;
